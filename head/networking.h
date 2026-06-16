31602fd56811d7aec04f077ac44e41e81e707964
NekoMimi <nekomimiofficial@proton.me>
2026-06-16 13:59:50 +0300

    made network platform dependant

--- head/networking.h
+++ head/networking.h
@@ -1,13 +1,4 @@
 #pragma once
-#include <cstddef>
 #include <string>
-#include <curl/curl.h>
 
-static std::size_t WriteCallback(void* contents, std::size_t size, std::size_t nmemb, void* userp) {
-  size_t totalSize = size * nmemb;
-  std::string* str = static_cast<std::string*>(userp);
-  str->append(static_cast<char*>(contents), totalSize);
-  return totalSize;
-}
-
-std::string doGetRequest(const std::string& url);
+std::string doGetRequest(const std::string &url);
--- src/linux/networking.cpp
+++ src/linux/networking.cpp
@@ -3,6 +3,14 @@
 #include <iostream>
 #include <string>
 
+static std::size_t WriteCallback(void *contents, std::size_t size,
+                                 std::size_t nmemb, void *userp) {
+  size_t totalSize = size * nmemb;
+  std::string *str = static_cast<std::string *>(userp);
+  str->append(static_cast<char *>(contents), totalSize);
+  return totalSize;
+}
+
 std::string doGetRequest(const std::string &url) {
   CURL *curl;
   CURLcode res;
