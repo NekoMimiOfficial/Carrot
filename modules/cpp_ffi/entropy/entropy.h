96f8678463836a2afd23b17f5fe261f5b80bf727
Deva Midhun <admin@coder.is-a.dev>
2026-07-16 02:23:01 +0530

    added apache2 notice

--- modules/cpp_ffi/entropy/entropy.h
+++ modules/cpp_ffi/entropy/entropy.h
@@ -1,10 +1,19 @@
 /*
  * This file is part of the Carrot/Ninjin project.
  *
- * This module is dual-licensed under the Nicense Software License v1.1
- * AND the Apache License, Version 2.0. You may choose either license.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * For the full terms of the Nicense, see the LICENSE.nicense file in this directory.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Deva Midhun
  */
 
 // This file was taken from the ZNU Kernel Source
